15e6126e35165de42939082916f36a03e2ef3ac9
NhanN <NhanN@fpt.com.vn>
2024-07-30 02:08:06 +0700

    update

--- src/app/components/book-detail/phieu-bai-tap/phieu-bai-tap.component.ts
+++ src/app/components/book-detail/phieu-bai-tap/phieu-bai-tap.component.ts
@@ -1,12 +1,342 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileData, FileService } from '../../../services/file.service';
+import { ActivatedRoute } from '@angular/router';
+import { PhieuBaiTapButtonComponent } from '../../buttons/phieu-bai-tap-button/phieu-bai-tap-button.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-phieu-bai-tap',
   standalone: true,
-  imports: [],
+  imports: [PhieuBaiTapButtonComponent, CommonModule, FormsModule],
   templateUrl: './phieu-bai-tap.component.html',
-  styleUrl: './phieu-bai-tap.component.css'
+  styleUrl: './phieu-bai-tap.component.css',
 })
-export class PhieuBaiTapComponent {
+export class PhieuBaiTapComponent implements OnInit {
+  bookName: string = '';
+  bookIconUrl: string = '';
+  folderPath: string = '';
+  // files: FileData[] = [];
 
+  files: any[] = [];
+  selectedSemester = 'hoc-ki-1';
+  options = [
+    { value: 'hoc-ki-1', label: 'Học kì 1' },
+    { value: 'hoc-ki-2', label: 'Học kì 2' },
+  ];
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly fileService: FileService
+  ) {}
+
+  ngOnInit(): void {
+    this.bookName = this.route.snapshot.paramMap.get('bookName')!;
+    this.bookIconUrl = `images/images/${this.bookName}-icon.jpg`;
+    this.route.pathFromRoot.forEach((route) => {
+      route.url.subscribe((urlSegment) => {
+        this.folderPath +=
+          '/' + urlSegment.map((segment) => segment.path).join('/');
+
+        // this.listFiles();
+        this.getListTempFiles();
+      });
+    });
+  }
+
+  onChangeSemester() {
+    if (this.selectedSemester == 'hoc-ki-2') {
+      this.files = [
+        {
+          name: 'Tuần 19',
+          url: '',
+        },
+        {
+          name: 'Tuần 20',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 21',
+          url: '',
+        },
+        {
+          name: 'Tuần 22',
+          url: '',
+        },
+        {
+          name: 'Tuần 23',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 24',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 25',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 26',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 27',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 28',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 29',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 30',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 31',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 32',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 33',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 34',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 35',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 36',
+          url: '',
+        },
+        {
+          name: 'Tuần 37',
+          url: '',
+        },
+      ];
+    } else {
+      this.files = [
+        {
+          name: 'Tuần 1',
+          url: '',
+        },
+        {
+          name: 'Tuần 2',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 3',
+          url: '',
+        },
+        {
+          name: 'Tuần 4',
+          url: '',
+        },
+        {
+          name: 'Tuần 5',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 6',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 7',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 8',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 9',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 10',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 11',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 12',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 13',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 14',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 15',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 16',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 17',
+          url: '',
+        },
+
+        {
+          name: 'Tuần 18',
+          url: '',
+        },
+      ];
+    }
+  }
+
+  getListTempFiles() {
+    this.files = [
+      {
+        name: 'Tuần 1',
+        url: '',
+      },
+      {
+        name: 'Tuần 2',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 3',
+        url: '',
+      },
+      {
+        name: 'Tuần 4',
+        url: '',
+      },
+      {
+        name: 'Tuần 5',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 6',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 7',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 8',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 9',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 10',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 11',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 12',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 13',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 14',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 15',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 16',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 17',
+        url: '',
+      },
+
+      {
+        name: 'Tuần 18',
+        url: '',
+      },
+    ];
+  }
+
+  listFiles(): void {
+    this.files = [];
+    this.fileService.getFilesList(this.folderPath).subscribe((files) => {
+      files.forEach((file: any) => {
+        this.files.push(file);
+      });
+    });
+  }
+
+  getTitle() {
+    if (this.bookName == 'canh-dieu') {
+      return 'Cánh diều';
+    } else if (this.bookName == 'chan-troi-sang-tao') {
+      return 'Chân trời sáng tạo';
+    } else {
+      return 'Kết nối tri thức';
+    }
+  }
 }
---
+++ src/app/components/buttons/ke-hoach-bai-day-button/ke-hoach-bai-day-button.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FileService } from '../../../services/file.service';
+import { Router } from 'express';
+
+@Component({
+  selector: 'app-ke-hoach-bai-day-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './ke-hoach-bai-day-button.component.html',
+  styleUrl: './ke-hoach-bai-day-button.component.css',
+})
+export class KeHoachBaiDayButtonComponent implements OnChanges {
+  @Input('buttonName') buttonName: string = '';
+  @Input('folderPath') folderPath: string = '';
+  imgUrl: string = 'images/images/';
+  constructor(
+    private readonly fileService: FileService,
+    private readonly router: Router
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['buttonName'] && this.buttonName?.length > 0) {
+      if (this.folderPath.includes('canh-dieu')) {
+        this.imgUrl += 'canh-dieu-icon.jpg';
+      } else if (this.folderPath.includes('ket-noi-tri-thuc')) {
+        this.imgUrl += 'ket-noi-tri-thuc-icon.jpg';
+      } else {
+        this.imgUrl += 'chan-troi-sang-tao-icon.jpg';
+      }
+    }
+  }
+
+  downloadFile(): void {
+    const link = document.createElement('a');
+    link.href = `/files/${this.folderPath}`; // Replace with your file path
+    link.download = `${this.buttonName}`; // Replace with the desired file name
+    link.click();
+  }
+}
