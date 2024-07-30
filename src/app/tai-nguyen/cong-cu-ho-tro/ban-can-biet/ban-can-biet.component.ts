8860c4694fa53b4a3f6c2d6f5ea991ddf9d274c9
NhanN <NhanN@fpt.com.vn>
2024-07-31 02:40:36 +0700

    update

--- src/app/tai-nguyen/cong-cu-ho-tro/ban-can-biet/ban-can-biet.component.ts
+++ src/app/tai-nguyen/cong-cu-ho-tro/ban-can-biet/ban-can-biet.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ban-can-biet',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ban-can-biet.component.html',
-  styleUrl: './ban-can-biet.component.css'
+  styleUrl: './ban-can-biet.component.css',
 })
-export class BanCanBietComponent {
+export class BanCanBietComponent implements OnInit {
+  imgUrls: string[] = [
+    'images/images/tai-nguyen/ban-can-biet/Slide1.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide2.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide3.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide4.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide5.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide6.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide7.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide8.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide9.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide10.PNG',
+    'images/images/tai-nguyen/ban-can-biet/Slide11.PNG',
+  ];
 
+  currentUrl: number = 0;
+
+  ngOnInit(): void {}
+
+  onClick(event: number) {
+    this.currentUrl += event;
+    if (this.currentUrl < 0) {
+      this.currentUrl = this.imgUrls.length - 1;
+    } else if (this.currentUrl > this.imgUrls.length - 1) {
+      this.currentUrl = 0;
+    }
+  }
 }
