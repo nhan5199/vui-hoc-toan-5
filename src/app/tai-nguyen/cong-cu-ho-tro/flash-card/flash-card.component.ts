3c10ce3290f5449b05f21a4b011b98cdd32763fc
NhanN <NhanN@fpt.com.vn>
2024-08-02 02:22:04 +0700

    update

--- src/app/tai-nguyen/cong-cu-ho-tro/flash-card/flash-card.component.ts
+++ src/app/tai-nguyen/cong-cu-ho-tro/flash-card/flash-card.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileData, FileService } from '../../../services/file.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-flash-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './flash-card.component.html',
-  styleUrl: './flash-card.component.css'
+  styleUrl: './flash-card.component.css',
 })
-export class FlashCardComponent {
+export class FlashCardComponent implements OnInit {
+  currentPath: string = '';
+  files: FileData[] = [];
 
+  isDisplayViewSlide: boolean = false;
+  slideName: string = '';
+  slideDownloadUrl: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private fileService: FileService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.pathFromRoot.forEach((route) => {
+      route.url.subscribe((urlSegment) => {
+        this.currentPath +=
+          '/' + urlSegment.map((segment) => segment.path).join('/');
+
+        this.listFiles();
+      });
+    });
+  }
+
+  // List files
+  listFiles(): void {
+    this.files = [];
+    this.fileService
+      .getFilesList(this.currentPath.split('//')[1])
+      .subscribe((files) => {
+        files.forEach((file: any) => {
+          this.files.push(file);
+        });
+        console.log(this.files);
+      });
+  }
 }
