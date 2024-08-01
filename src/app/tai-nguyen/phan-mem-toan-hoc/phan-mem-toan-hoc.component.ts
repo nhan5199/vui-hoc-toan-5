13fe420b7083fa57afc065d4f6578107bdacb16e
NhanN <NhanN@fpt.com.vn>
2024-08-01 19:37:33 +0700

    update

--- src/app/tai-nguyen/phan-mem-toan-hoc/phan-mem-toan-hoc.component.ts
+++ src/app/tai-nguyen/phan-mem-toan-hoc/phan-mem-toan-hoc.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileData, FileService } from '../../services/file.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-phan-mem-toan-hoc',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './phan-mem-toan-hoc.component.html',
-  styleUrl: './phan-mem-toan-hoc.component.css'
+  styleUrl: './phan-mem-toan-hoc.component.css',
 })
-export class PhanMemToanHocComponent {
+export class PhanMemToanHocComponent implements OnInit {
+  folderPath: string = '';
+  files: FileData[] = [];
+  constructor(
+    private readonly fileService: FileService,
+    private readonly route: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.route.pathFromRoot.forEach((route) => {
+      route.url.subscribe((urlSegment) => {
+        this.folderPath +=
+          '/' + urlSegment.map((segment) => segment.path).join('/');
+
+        this.listFiles();
+      });
+    });
+  }
+
+  listFiles(): void {
+    this.files = [];
+    this.fileService.getFilesList(this.folderPath).subscribe((files) => {
+      files.forEach((file: any) => {
+        this.files.push(file);
+      });
+    });
+  }
 }
